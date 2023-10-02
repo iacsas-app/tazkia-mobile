70c225d402cec6227ec5ed1f9ae5e06b8c7f0376
IACSAS <iacsasapp@gmail.com>
2023-10-02 17:59:29 +0200

    feature(purification): evaluation

--- src/components/rules/CheckableRule.tsx
+++ src/components/rules/CheckableRule.tsx
@@ -1,5 +1,5 @@
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
-import { Avatar, HStack, Pressable, Text } from '@react-native-material/core';
+import { Avatar, Box, HStack, Pressable, Text } from '@react-native-material/core';
 import { useState } from 'react';
 import { RuleStyles } from './Styles';
 import { RuleBaseProps } from './types';
@@ -14,19 +14,33 @@ export default function CheckableRule({ id, item, reverse }: RuleProps) {
   }
 
   return (
-    <Pressable onPress={handlePress}>
-      <HStack spacing={10} style={RuleStyles.container} reverse={reverse}>
-        <Avatar
-          label={checked ? undefined : <Text variant="subtitle2">{id}</Text>}
-          size={25}
-          color={checked ? 'error' : 'secondary'}
-          tintColor="white"
-          icon={(props) => <Icon name="check-bold" {...props} />}
-        />
-        <Text variant="body1" style={RuleStyles.text}>
-          {item}
-        </Text>
-      </HStack>
+    <Pressable
+      onPress={handlePress}
+      style={{
+        ...RuleStyles.pressable,
+        backgroundColor: checked ? '#ffb6c1' : 'white',
+      }}
+    >
+      <Box style={RuleStyles.box}>
+        <HStack spacing={10} style={RuleStyles.stack} reverse={reverse}>
+          <Avatar
+            label={
+              checked ? undefined : (
+                <Text variant="subtitle2" color="white">
+                  {id}
+                </Text>
+              )
+            }
+            size={25}
+            color={checked ? 'error' : '#228b22'}
+            tintColor="white"
+            icon={(props) => <Icon name="check-bold" {...props} />}
+          />
+          <Text variant="body1" style={RuleStyles.text}>
+            {item}
+          </Text>
+        </HStack>
+      </Box>
     </Pressable>
   );
 }
